121c10e58a91ec55a4757bf828876ff59179d5f3
Abe <abramabradley@gmail.com>
2021-03-28 04:06:25 -0400

    changes

---
+++ public/app.js
@@ -0,0 +1,63 @@
+const pad = value => (value < 10 ? '0' + value : value)
+
+class Clock {
+  constructor(node) {
+    this.node = node
+    this.init()
+  }
+  init() {
+    let time = +localStorage.getItem('time') ?? 0
+
+    this.time = time
+    this.hours = Math.floor(time / 3600)
+    time %= 3600
+
+    this.minutes = Math.floor(time / 60)
+    time %= 60
+
+    this.seconds = time
+
+    this.set()
+  }
+  tick = () => {
+    this.time += 1
+    localStorage.setItem('time', this.time)
+    this.seconds += 1
+    if (this.seconds >= 60) {
+      this.seconds = 0
+      this.minutes += 1
+      if (this.minutes >= 60) {
+        this.minutes = 0
+        this.hours += 1
+      }
+    }
+    this.set()
+  }
+  set() {
+    this.node.innerText = `${pad(this.hours)}:${pad(this.minutes)}:${pad(
+      this.seconds
+    )}`
+  }
+  floor = () => {
+    this.time %= 3600
+    localStorage.setItem('time', this.time)
+    this.init()
+  }
+}
+
+const startButton = document.querySelector('#start')
+const floorButton = document.querySelector('#floor')
+const clock = new Clock(document.querySelector('#display'))
+
+let interval
+startButton.onclick = () => {
+  if (!interval) {
+    startButton.innerText = 'stop'
+    interval = setInterval(clock.tick, 1000)
+  } else {
+    startButton.innerText = 'start'
+    clearInterval(interval)
+  }
+}
+
+floorButton.onclick = clock.floor
